b082a5dc08256cef9a40b47554e20b309c38326c
MisaOgura <misa.ogura01@gmail.com>
2018-09-18 17:56:05 +0100

    Construct params dynamically from use input

--- news_api.py
+++ news_api.py
@@ -7,28 +7,18 @@ import datetime
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Load environmental variables from .env file
-
 env_path = Path('./') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 class NewsApi():
-    def __init__(self, query_term, output, endpoint='everything'):
-        self._api_key = os.getenv('API_KEY')
-        self._pagesize = 100
+    def __init__(self, endpoint, output_filename, **kwargs):
         self._base_url = 'https://newsapi.org/v2/{}'.format(endpoint)
+        self._output_filename = output_filename
+        self._params = {**kwargs, 'apiKey': os.getenv('API_KEY')}
 
-        self._output = output
-
-        self._data = {'query_term': query_term,
+        self._data = {'custom_params': {**kwargs},
                       'datetime': str(datetime.datetime.now()),
                       'total_articles': 0}
-
-        self._params = {'q': query_term,
-                        'apiKey': self._api_key,
-                        'pageSize': self._pagesize,
-                        'language': 'en',
-                        'sortBy': 'publishedAt'}
 
     def _query(self, current_page):
         print('\nGrabbing page: {}...'.format(current_page))
@@ -38,7 +28,7 @@ class NewsApi():
         response = requests.get(self._base_url, params=self._params)
 
         if response.status_code != 200:
-            print('\nRequest failed with: ', response.status_code)
+            print('\nRequest failed with a status code: {}'.format(response.status_code))
             print('\nDetails: ', response.text)
             self._cleanup()
         else:
@@ -51,12 +41,17 @@ class NewsApi():
                 return data
 
     def _calculate_total_page(self, total_result):
+        page_size = 20
+
+        if 'pageSize' in self._params:
+            page_size = self._params['pageSize']
+
         if total_result <= 100:
             return 1
-        elif total_result % self._pagesize == 0:
-            return total_result / self._pagesize
+        elif total_result % page_size == 0:
+            return total_result / page_size
         else:
-            return int(total_result / self._pagesize) + 1
+            return int(total_result / page_size) + 1
 
     def _save(self):
         total_articles = len(self._data['articles'])
@@ -66,14 +61,13 @@ class NewsApi():
 
         self._data['total_articles'] = total_articles
 
-        with open(self._output, 'w') as file:
+        with open(self._output_filename, 'w') as file:
             json.dump(self._data, file, indent=4)
 
-        print('\nData saved in {}!'.format(output_file))
+        print('\nData saved in {}!'.format(self._output_filename))
 
     def _main(self):
         current_page = 1
-
         response = self._query(current_page)
 
         total_result = response['totalResults']
@@ -84,14 +78,10 @@ class NewsApi():
         print('\nTotal number of pages to paginate: {}'.format(total_page))
 
         while current_page < total_page:
-            # Obtain next page from the API
-
             current_page += 1
-            new_data = self._query(current_page)
+            response = self._query(current_page)
 
-            # Merge new articles to the obtained list of articles
-
-            self._data['articles'].extend(new_data['articles'])
+            self._data['articles'].extend(response['articles'])
 
         self._save()
 
@@ -106,13 +96,56 @@ class NewsApi():
             print('\nDetected SIGINT!!!')
             self._cleanup()
 
+def create_params_from_input(keys):
+    params = {}
+
+    for key in keys:
+        value = input('Enter a value for "{}": '.format(key))
+
+        if value != '':
+            if key == 'pageSize':
+                params[key] = int(value)
+            else:
+                params[key] = value
+
+    return params
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print('Usage: news_api.py <query_term> <output_file>')
-        print('e.g. news_api.py "gender AND tech" my_data.txt')
+        print('Usage: news_api.py <endpoint> <output_filename>')
+        print('e.g. news_api.py everything test.txt')
         exit(1)
-    else:
-        query = sys.argv[1]
-        output_file = sys.argv[2]
 
-        NewsApi(query, output_file).execute()
+    endpoint = sys.argv[1]
+    output_filename = sys.argv[2]
+
+    if os.path.exists(output_filename):
+        print('The file {} already exists. Please provide another filename.'.format(output_filename))
+        exit(1)
+
+    params = {}
+
+    if endpoint == 'everything':
+        keys = ['q',
+                'sources',
+                'domains',
+                'from',
+                'to',
+                'language',
+                'sortBy',
+                'pageSize']
+
+    elif endpoint == 'top-headlines':
+        keys = ['q',
+                'sources',
+                'category',
+                'language',
+                'country',
+                'pageSize']
+
+    print('\nConstructing query parameters for /{} endpoint...'.format(endpoint))
+    print('Documentation (default/available values etc.): https://newsapi.org/docs/endpoints/{}\n'.format(endpoint))
+
+    params = create_params_from_input(keys)
+
+    NewsApi(endpoint, output_filename, **params).execute()
